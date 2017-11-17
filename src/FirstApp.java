5b8ae0520f2d73f3c938c0b72c0a124765f02b08
dsv80 <32156319+dsv80@users.noreply.github.com>
2017-11-17 23:54:12 +0700

    Create FirstApp.java

---
+++ src/FirstApp.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class FirstApp {
+
+    public static void main(String[] args) {
+       for (int i=0; i<args.length; i++) { // Проверка количества ячеек в массиве
+           System.out.println(args[i]); // Вывод параметров
+       }
+        /* ArrayList<User> users = new ArrayList<>(); //Создание юзера
+
+        User a1 = new User("sdfg", "portg455", 1);
+        users.add(a1); // Добавление в коллекцию
+
+        User a2 = new User("sdfg23", "portg455", 2);
+        users.add(a2); // Добавление в коллекцию
+
+        User a3 = new User("sdfg45", "portg455", 3);
+        users.add(a3); // Добавление в коллекцию
+        */
+        }
+}
